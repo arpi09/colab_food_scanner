8e7077c6992c4d3ff0e03ef038733a5e3957704c
Arvid <arvid.pilhall@me.com>
2020-06-07 13:37:52 +0200

    api key in env file

--- migration_script.py
+++ migration_script.py
@@ -1,15 +1,21 @@
 import requests, json
+from dotenv import load_dotenv
+import urllib.request, json 
+import os
+
+
+
 
 def main():
-    import urllib.request, json 
-    # with urllib.request.urlopen("http://api.dabas.com/DABASService/V2/article/gtin/05053990138722?apikey=bbb3c928-8c54-4ac6-88ef-7543c246d1ca") as url:
-    with urllib.request.urlopen("http://api.dabas.com/DABASService/V2/articles?apikey=bbb3c928-8c54-4ac6-88ef-7543c246d1ca") as url:
+    load_dotenv()
+    with urllib.request.urlopen("http://api.dabas.com/DABASService/V2/articles?apikey=" + os.getenv('DABAS_API_KEY')) as url:
         data = json.loads(url.read().decode())
 
         # print(json.dumps(data, indent=4, sort_keys=True, separators=(',', ': ')))
         print(data[0]["GTIN"])
         for gtin in data:
             print(gtin["GTIN"])
+
 
 if __name__ == "__main__":
     main()
